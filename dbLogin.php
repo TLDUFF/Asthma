d99409b36399f66b9e10efec99899968cba690e1
TLDUFF <TLDUFF@users.noreply.github.com>
2014-01-15 10:31:42 -0800

    Create dbLogin.php
    shell of dbLogin.php used by addParticipant.php

---
+++ dbLogin.php
@@ -0,0 +1,19 @@
+<?php
+
+/* dbLogin.php
+ * 
+ * Date: 11 Dec. 2013
+ * Author: tduff@sdsc.edu
+ * About: login information to connect to the db - for use 
+ * with the Asthma / Survey Monkey project.  
+ * variables left empty on purpose, for this github version.
+ * 
+ * Required by addParticipant.php
+ */
+
+$db_hostname = '';
+$db_database = '';
+$db_username = '';
+$db_password = '';
+      
+?>
