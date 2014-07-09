e550ca4d2d3d4296922ee9eae5849a63288d1785
TLDUFF <TLDUFF@users.noreply.github.com>
2014-07-09 11:43:21 -0700

    Update and rename main_login.php to login_main.php
    update name of login page to reference.

--- login_main.php
+++ login_main.php
@@ -8,7 +8,7 @@
 
 <table width="300" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
     <tr>
-    <form name="form1" method="post" action="checklogin.php">
+    <form name="form1" method="post" action="login_check.php">
         <td>
             <table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
             <tr>
