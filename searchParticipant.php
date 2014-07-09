7daab6c33cf60842000ef4b14ed53a5ed70c8f6a
TLDUFF <TLDUFF@users.noreply.github.com>
2014-07-09 11:48:03 -0700

    Update searchParticipant.php
    Latest working version as of July 9, 2014

--- searchParticipant.php
+++ searchParticipant.php
@@ -1,61 +1,63 @@
-
-
-<script type="text/javascript">
-            function searchParticipants(){
-                document.forms['asthma_participants'].action = 'searchParticipants.php';
-                document.forms['asthma_participants'].submit();
-            }
-        </script>
-
- <input type="button" name="search" value="FIND RECORD" onclick="searchParticipants();" />
- 
 <?php
+//Update DB tables as $_POST vars are set.
+    require_once('./dbLogin.php');
 
-//require_once('dbLogin.php');
-//
-//// (1) Open the Database Connection
-//$db_server = mysql_connect($db_hostname, $db_username, $db_password);
-//
-//if (!$db_server) die("Unable to connect to MySQL: " . mysql_error());
-//
-////(2) Select the asthmadb
-//mysql_select_db($db_database, $db_server)
-//        or die("Unable to select database: " . mysql_error());
+   $db = new Database();
+   $db->Connect();
 
-// QUERY participants table  TDUFF CHANGE THIS LOGIC
-if (isset($_POST['email']) &&
-    isset($_POST['pregid']))
-{
-    $email = get_post('email');
-    $pregid = get_post('pregid');
-    
-    $query = "SELECT email, pregid, survey_link FROM participants" .
-            "WHERE email =  $email ".
-            "AND pregid =  $pregid ";
-    
-    if (mysql_query($query, $db_server)) {
-        echo '<div id="container">';
-        echo '<br />';
-        echo "The following link was found for $email with PregID $pregid";
-        echo '<br /><br />';
-        
-        echo '<br /><br />';
-        echo '</div>';
-    } else {
-        echo '<div id="container">';
-        echo '<br />';
-        echo '<div id="error"> QUERY FAILED: </div>' . " $query<br /><br />" .
-            mysql_error() . "<br /><br />";
-        echo '</div>';
-    }// end if/else 
-} else {
-    echo "Please fill in both E-mail and PregID.";
-}// end if isset
+// Search by email
+    if (isset ($_POST['email'])) {
 
-mysql_close($db_server);
+       $email = trim($_POST['email']);
+       $sql = "SELECT survey_link
+               FROM participants
+               WHERE email = $email";
+
+           if (!($db->Query($sql))) {
+             echo mysql_error($sql);
+           }
+        header('Location:./addParticipant.php');
+    }
+
+
+// Search by PregID
+    if(isset($_POST['pregid']) && isset($_POST['pregid2'])){
+
+        if (($_POST['pregid']) === ($_POST['pregid2'])) {
+
+            // pregid is only numbers, and no more than 11 digits.
+            if (preg_match( "/^[0-9]{1,11}$/", $_POST['pregid']))
+            {
+                $pregid = get_post('pregid');
+
+                $sql = "SELECT survey_link
+                    FROM participants
+                    WHERE pregid = $pregid";
+
+                if (!($db->Query($sql))) {
+                     echo mysql_error($sql);
+                }
+
+                header('Location:./addParticipant.php');
+
+            } else {
+                echo '<div class="error">PregID is one to eleven digits, numbers only. </div>';
+                // clear vars for re-entry
+                $pregid = '';
+                exit;
+            }
+        } else { // PregIDs do not match
+            echo '<div class="error">PregIds do not match. Please re-enter. </div>';
+            // clear vars for re-entry
+            $email = $pregid = $pregid2 = $survey_link = $query = '';
+        }// end if PREGIDs match
+
+    }// end isset
+
 
 function get_post($var)
 {
     return mysql_real_escape_string($_POST[$var]);
 }
+
 ?>
